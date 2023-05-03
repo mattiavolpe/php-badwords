e45909ff13bc55d126d8e852727198dc6ad99d31
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-05-03 14:17:53 +0200

    FIRST commit with form for input

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- EXTERNAL BOOTSTRAP CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        
+    <title>Bad Words Inputs</title>
+  </head>
+  
+  <body>
+    
+    <div class="container">
+      <div class="row my-5">
+        <div class="col-6 mx-auto">
+          <form action="myscript.php" method="get">
+            <div class="mb-3">
+              <label for="sentence" class="form-label">Insert sentence</label>
+              <input type="text" name="sentence" id="sentence" class="form-control" placeholder="Insert sentence here...">
+            </div>
+            <div class="mb-3">
+              <label for="bad_word" class="form-label">Insert word to censor</label>
+              <input type="text" name="bad_word" id="bad_word" class="form-control" placeholder="Insert word to censor here...">
+            </div>
+            <div class="mb-3">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    
+  </body>
+  
+</html>
